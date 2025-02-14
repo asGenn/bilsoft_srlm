28e1b78242da48b555b543aae9c804e8acbde6ba
Abdulsamet Genç <abdulsametgencc@gmail.com>
2025-02-14 16:46:17 +0300

    Add flutter_local_notifications package for local notifications support

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:bilsoft_srlm/core/di/service_locator.dart';
-
-
 import 'package:bilsoft_srlm/features/splash/splash.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await setup();
-  
+
   runApp(const MyApp());
 }
 
